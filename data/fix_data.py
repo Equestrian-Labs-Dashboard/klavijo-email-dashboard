6166ba55cb1fcff23f103f77ba01e2fa13ea8890
Equestrian Labs <arojas@corroshop.com>
2026-09-08 03:50:20 -0400

    Add files via upload

---
+++ data/fix_data.py
@@ -0,0 +1,24 @@
+﻿import json
+
+with open('data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+months_to_add = ['sep-26', 'oct-26', 'nov-26', 'dic-26']
+data['months'].extend(months_to_add)
+
+def pad_array(arr):
+    if isinstance(arr, list):
+        arr.extend([None, None, None, None])
+
+pad_array(data['gross_sales'])
+pad_array(data['active_profiles'])
+pad_array(data['active_base_growth_pct'])
+
+for key in data['campaigns']:
+    pad_array(data['campaigns'][key])
+
+for key in data['flows']:
+    pad_array(data['flows'][key])
+
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=2, ensure_ascii=False)
